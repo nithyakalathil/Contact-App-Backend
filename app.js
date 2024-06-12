8067ea5d7e24beb7f38ef8c731249cbcdc25e7a7
nithyakalathil <nithya913k@gmail.com>
2024-06-12 23:27:01 +0530

    create

---
+++ app.js
@@ -0,0 +1,24 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const cors=require("cors")
+const{contactmodel}=require("./models/contact")
+
+const app=express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://nithya:nithya913@cluster0.r7eo1il.mongodb.net/coursesdb?retryWrites=true&w=majority&appName=Cluster0")
+
+
+app.post("/add",(req,res)=>{
+    let input=req.body
+    let contact=new contactmodel(input)
+    contact.save()
+    console.log(contact)
+    res.json({status:"Success"})
+})
+
+
+app.listen(8080,()=>{
+    console.log("server started")
+})
